36b069c452c7db0e60838a6cd9fdf5fb1c220c2c
MarcoAntonioAS17 <antonioarcosmarco@gmail.com>
2021-10-12 00:40:49 -0500

    Get al backend

--- dh-lifecycle-react/src/components/ContentRowMovies.js
+++ dh-lifecycle-react/src/components/ContentRowMovies.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SmallCard from './SmallCard';
 
 let productInDataBase = {
@@ -26,10 +26,19 @@ let cardProps = [productInDataBase,amount,user];
 
 
 function ContentRowTop(){
+    const [productos, setProductos] = useState({});
+
+    useEffect(()=> {
+        fetch('http://localhost:3003/api/products')
+        .then(response => response.json())
+        .then(data => setProductos(data));
+    },[]);
+
     return (
         <React.Fragment>
         {/*<!-- Content Row -->*/}
         <div className="row">
+            <SmallCard color="success" titulo="Total productos" valor={productos.count} icono="fas fa-award"/>
             {
                 cardProps.map((producto,index)=>{
                     return <SmallCard  {...producto}  key= {index}/>
